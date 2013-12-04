47a86326185ab5fbd891aeb99e4b239d3513020b
najlepsiwebdesigner <najlepsiwebdesigner@gmail.com>
2013-12-04 09:12:57 +0000

    toggle threshold and depth windows, initial start with just RGB window

--- my_kinect.py
+++ my_kinect.py
@@ -8,50 +8,13 @@ import numpy as np
 
 threshold = 100
 current_depth = 0
-
 depth_image = 0;
+
+
 depth_window = 0;
+threshold_window = 0;
 
-
-#def save_depth_image(iplimage):
-#	global depth_image
-#	depth_image = iplimage
-
-def change_threshold(value):
-    global threshold
-    threshold = value
-    print 'threshold changed to: {0}'.format(threshold)
-
-def change_depth(value):
-    global current_depth
-    current_depth = value
-    print 'depth changed to: {0}'.format(current_depth)
-
-def show_depth():
-    global threshold
-    global current_depth
-    global depth_image
-    
-    depth, timestamp = freenect.sync_get_depth()
-    
-    depth_image =  frame_convert.pretty_depth_cv(depth);
-    
- #   depth = 255 * np.logical_and(depth >= current_depth - threshold,
- #                                depth <= current_depth + threshold)
- #   depth = depth.astype(np.uint8)
- #   threshold_image = cv.CreateImageHeader((depth.shape[1], depth.shape[0]),
- #                                cv.IPL_DEPTH_8U,
- #                                1)
- #   cv.SetData(threshold_image, depth.tostring(),
- #             depth.dtype.itemsize * depth.shape[1])
- #   cv.ShowImage('Threshold', threshold_image)
-
-    cv.ShowImage('Depth', depth_image) 
-
-def show_video():
-    cv.ShowImage('Video', frame_convert.video_cv(freenect.sync_get_video()[0]))
-
-
+# OPENCV GUI callbacks
 def toggle_depth_window(value):
     global depth_window
     
@@ -63,22 +26,85 @@ def toggle_depth_window(value):
         cv.NamedWindow('Depth')
         depth_window = 1
     
+def toggle_threshold_window(value):
+    global threshold_window
+    
+    #window is opened and has to be closed
+    if threshold_window == 1 and value == 0:
+        cv.DestroyWindow('Threshold')
+        threshold_window = 0
+    elif threshold_window == 0 and value == 1:
+        cv.NamedWindow('Threshold')
+        cv.CreateTrackbar('threshold', 'Threshold', threshold,     500,  change_threshold)
+        cv.CreateTrackbar('depth',     'Threshold', current_depth, 2048, change_depth)
+        threshold_window = 1
 
-#cv.NamedWindow('Depth')
+def change_threshold(value):
+    global threshold
+    threshold = value
+#    print 'threshold changed to: {0}'.format(threshold)
+
+def change_depth(value):
+    global current_depth
+    current_depth = value
+#    print 'depth changed to: {0}'.format(current_depth)
+
+
+
+
+
+
+
+
+# window content rendering section
+def show_depth():
+    global depth_image
+    
+    depth, timestamp = freenect.sync_get_depth()
+    depth_image =  frame_convert.pretty_depth_cv(depth);
+    cv.ShowImage('Depth', depth_image) 
+
+def show_video():
+    cv.ShowImage('Video', frame_convert.video_cv(freenect.sync_get_video()[0]))
+
+
+def show_threshold():
+    global threshold
+    global current_depth
+
+    depth, timestamp = freenect.sync_get_depth()
+    depth = 255 * np.logical_and(depth >= current_depth - threshold,
+                                 depth <= current_depth + threshold)
+    depth = depth.astype(np.uint8)
+    threshold_image = cv.CreateImageHeader((depth.shape[1], depth.shape[0]),
+                                 cv.IPL_DEPTH_8U,
+                                 1)
+    cv.SetData(threshold_image, depth.tostring(),
+              depth.dtype.itemsize * depth.shape[1])
+    cv.ShowImage('Threshold', threshold_image)
+
+
+
+
+# open initial window with controls to open additional windows - opencv doesnt have buttons, so we use trackbars
 cv.NamedWindow('Video')
-#cv.NamedWindow('Threshold')
-#cv.CreateTrackbar('threshold', 'Threshold', threshold,     500,  change_threshold)
-#cv.CreateTrackbar('depth',     'Threshold', current_depth, 2048, change_depth)
-
 cv.CreateTrackbar('Depth Window', 'Video', depth_window, 1, toggle_depth_window)
+cv.CreateTrackbar('Threshold Window', 'Video', threshold_window, 1, toggle_threshold_window)
 
+
+#print welcome message to command line
 print('MyKinect v0.0.1 \nPress ESC in window to stop, \npress "s" key to save RGB image, \npress"d" key to save DEPTH image.')
 
+# main program loop
 while 1:
     if depth_window:
         show_depth()
-        
+    
+    if threshold_window:
+        show_threshold();
+    
     show_video()
+    
     key = cv.WaitKey(5) & 0xFF
     if key == 27:
         break;
