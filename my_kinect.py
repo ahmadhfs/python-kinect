e4063a5f9bba2cdc32fd58b8715da239af456ba8
najlepsiwebdesigner <najlepsiwebdesigner@gmail.com>
2013-12-04 09:00:12 +0000

    initial commit of new repo

---
+++ my_kinect.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+import freenect
+import cv
+import cv2
+import frame_convert
+import numpy as np
+
+
+threshold = 100
+current_depth = 0
+
+depth_image = 0;
+depth_window = 0;
+
+
+#def save_depth_image(iplimage):
+#	global depth_image
+#	depth_image = iplimage
+
+def change_threshold(value):
+    global threshold
+    threshold = value
+    print 'threshold changed to: {0}'.format(threshold)
+
+def change_depth(value):
+    global current_depth
+    current_depth = value
+    print 'depth changed to: {0}'.format(current_depth)
+
+def show_depth():
+    global threshold
+    global current_depth
+    global depth_image
+    
+    depth, timestamp = freenect.sync_get_depth()
+    
+    depth_image =  frame_convert.pretty_depth_cv(depth);
+    
+ #   depth = 255 * np.logical_and(depth >= current_depth - threshold,
+ #                                depth <= current_depth + threshold)
+ #   depth = depth.astype(np.uint8)
+ #   threshold_image = cv.CreateImageHeader((depth.shape[1], depth.shape[0]),
+ #                                cv.IPL_DEPTH_8U,
+ #                                1)
+ #   cv.SetData(threshold_image, depth.tostring(),
+ #             depth.dtype.itemsize * depth.shape[1])
+ #   cv.ShowImage('Threshold', threshold_image)
+
+    cv.ShowImage('Depth', depth_image) 
+
+def show_video():
+    cv.ShowImage('Video', frame_convert.video_cv(freenect.sync_get_video()[0]))
+
+
+def toggle_depth_window(value):
+    global depth_window
+    
+    #window is opened and has to be closed
+    if depth_window == 1 and value == 0:
+        cv.DestroyWindow('Depth')
+        depth_window = 0
+    elif depth_window == 0 and value == 1:
+        cv.NamedWindow('Depth')
+        depth_window = 1
+    
+
+#cv.NamedWindow('Depth')
+cv.NamedWindow('Video')
+#cv.NamedWindow('Threshold')
+#cv.CreateTrackbar('threshold', 'Threshold', threshold,     500,  change_threshold)
+#cv.CreateTrackbar('depth',     'Threshold', current_depth, 2048, change_depth)
+
+cv.CreateTrackbar('Depth Window', 'Video', depth_window, 1, toggle_depth_window)
+
+print('MyKinect v0.0.1 \nPress ESC in window to stop, \npress "s" key to save RGB image, \npress"d" key to save DEPTH image.')
+
+while 1:
+    if depth_window:
+        show_depth()
+        
+    show_video()
+    key = cv.WaitKey(5) & 0xFF
+    if key == 27:
+        break;
+    elif key == 115:
+		print '"s" key pressed!'
+		cv2.imwrite('RGB.jpg', freenect.sync_get_video()[0]);
+    elif key == 100 and depth_window:
+        print '"d" key pressed!'
+        cv.SaveImage('DEPTH.jpg', depth_image)
