9111f7e1275cba4e9c9ed66200577494978723a1
Peter Beňo <najlepsiwebdesigner@gmail.com>
2014-01-28 17:21:49 +0100

    add grayscale conversion and prepare for detection

--- my_kinect.py
+++ my_kinect.py
@@ -14,6 +14,68 @@ depth_image = 0;
 # windows and their initial states
 depth_window = 0;
 threshold_window = 0;
+detector_window = 1;
+
+
+# cascade classifiers
+face_cascade = cv2.CascadeClassifier('opencv_data/haarcascades/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('opencv_data/haarcascades/haarcascade_eye.xml')
+
+
+
+
+
+
+
+
+
+
+def cv2array(im): 
+  depth2dtype = { 
+        cv.IPL_DEPTH_8U: 'uint8', 
+        cv.IPL_DEPTH_8S: 'int8', 
+        cv.IPL_DEPTH_16U: 'uint16', 
+        cv.IPL_DEPTH_16S: 'int16', 
+        cv.IPL_DEPTH_32S: 'int32', 
+        cv.IPL_DEPTH_32F: 'float32', 
+        cv.IPL_DEPTH_64F: 'float64', 
+    } 
+
+  arrdtype=im.depth 
+  a = np.fromstring( 
+         im.tostring(), 
+         dtype=depth2dtype[im.depth], 
+         count=im.width*im.height*im.nChannels) 
+  a.shape = (im.height,im.width,im.nChannels) 
+  return a 
+
+def array2cv(a): 
+  dtype2depth = { 
+        'uint8':   cv.IPL_DEPTH_8U, 
+        'int8':    cv.IPL_DEPTH_8S, 
+        'uint16':  cv.IPL_DEPTH_16U, 
+        'int16':   cv.IPL_DEPTH_16S, 
+        'int32':   cv.IPL_DEPTH_32S, 
+        'float32': cv.IPL_DEPTH_32F, 
+        'float64': cv.IPL_DEPTH_64F, 
+    } 
+  try: 
+    nChannels = a.shape[2] 
+  except: 
+    nChannels = 1 
+  cv_im = cv.CreateImageHeader((a.shape[1],a.shape[0]), 
+dtype2depth[str(a.dtype)], nChannels) 
+  cv.SetData(cv_im, a.tostring(),a.dtype.itemsize*nChannels*a.shape[1]) 
+  return cv_im 
+
+
+
+
+
+
+
+
+
 
 # OPENCV GUI callbacks
 def toggle_depth_window(value):
@@ -77,6 +139,15 @@ def show_video():
     image = frame_convert.video_cv(freenect.sync_get_video()[0]);
     cv.ShowImage('Video', resize_image(image))
 
+def show_detector():
+    image = frame_convert.video_cv(freenect.sync_get_video()[0]);
+    # print(type(image))
+
+    gray = cv2.cvtColor(cv2array(image), cv2.COLOR_BGR2GRAY);
+
+    # print(type(gray));
+    cv.ShowImage('Detector', array2cv(gray))
+
 
 def show_threshold():
     global threshold
@@ -125,15 +196,20 @@ cv.NamedWindow('Video')
 cv.CreateTrackbar('Depth Window', 'Video', depth_window, 1, toggle_depth_window)
 cv.CreateTrackbar('Threshold Window', 'Video', threshold_window, 1, toggle_threshold_window)
 
+cv.NamedWindow('Detector')
  
 
 # main program loop
 while 1:
+
     if depth_window:
         show_depth()
     
     if threshold_window:
         show_threshold();
+
+    if detector_window:
+        show_detector();
     
     show_video()
     
